e2b72a95c05b88ae2f2d743b1702ea573b9047a3
Alex Kyriacou <kyriacou@uni-wuppertal.de>
2021-09-09 09:49:13 +0200

    redesigned pleaides-job-sbatch, it now produces one job script per source position and saves these scripts to a directory, submit-jobs executes these scripts

--- pleiades-job-sbatch.py
+++ pleiades-job-sbatch.py
@@ -1,43 +1,56 @@
-import numpy as np
 import os
 import sys
-import datetime
 
-if len(sys.argv) != 3:
-    print("error!, you must supply a list of jobs to be run \n format should look like: \n python pleaides-job.sbathc.py job-list.txt jobname")
-    sys.exit()
+"""
+Information:
+This script creates a list a bash scripts to run parabolic equation simulation jobs on a slurm cluster
 
-joblist = str(sys.argv[1])
+Input:
+A list of Time Domain or Mono-Frequency PE simulations with a predefined refractive index profile, geometry and pulse, where each job is on a different line, and these correspond to difference source positions
 
-nNodes_min = int(8)
-nNodes_max = int(300)
+Output:
+For each line, a bash script (i.e. src=10.sh) is created, which contains the allocated nodes, partition, allowed time and memory allocated to each job
+"""
 
-nodeMemory = int(350) #need to guess this before hand, should be in MB
+#Input Arguments
+fname_in = sys.argv[1] #List of jobs to be executed
+path2jobs = sys.argv[2] #Directory to save bash scripts to
 
-jobname = str(sys.argv[2])
-days = int(3.)
-hours = int(0)
-minutes = int(0)
-seconds = int(0)
-partition = 'normal'
+#Cluster Settings
+NODES_MIN = 4
+NODES_MAX = 100
+PARTITION = 'long'
+DAYS = 3
+HOURS = 0
+MEMORY = 400 # in MB
 
-sbatch = "#SBATCH"
-fname = jobname + '.sh'
+def make_sbatch(jobline, fname, jobname, nNodes_min, nNodes_max, partition, days, hours, nodeMemory): #Make batch file to execute job
+    sbatch = "#SBATCH"
+    fout = open(fname, 'w+')
+    fout.write("#!/bin/sh\n")
 
-fout = open(fname, 'w+')
-fout.write("#!/bin/sh\n")
+    minutes = 0
+    seconds = 0
 
-fout.write(sbatch + " --job-name=" + jobname +"\n")
-fout.write(sbatch + " --partition=" + partition + "\n")
-fout.write(sbatch + " --time=" +str(days) + "-" + str(hours) + ":" + str(minutes) + ":" + str(seconds) + " # days-hours:minutes:seconds\n")
-fout.write(sbatch + " --nodes=" + str(nNodes_min) + "-" + str(nNodes_max) + "\n")
-fout.write(sbatch + " --mem-per-cpu=" + str(nodeMemory) + " # in MB\n")
+    fout.write(sbatch + " --job-name=" + jobname +"\n")
+    fout.write(sbatch + " --partition=" + partition + "\n")
+    fout.write(sbatch + " --time=" +str(days) + "-" + str(hours) + ":" + str(minutes) + ":" + str(seconds) + " # days-hours:minutes:seconds\n")
+    if nNodes_min == nNodes_max:
+        fout.write(sbatch + " --nodes=" + str(nNodes_min) + "\n")
+    else:
+        fout.write(sbatch + " --nodes=" + str(nNodes_min) + "-" + str(nNodes_max) + "\n")
+    fout.write(sbatch + " --mem-per-cpu=" + str(nodeMemory) + " # in MB\n")
+    fout.write(jobline)
 
-fin = open(joblist, "r+")
-for line in fin:
-    fout.write(line)
-fin.close()
-fout.close()
+    makeprogram = "chmod u+x " + fname
+    os.system(makeprogram)
 
-makeprogram = "chmod u+x " + fname
-os.system(makeprogram)
+if __name__ == "__main__":
+    fin = open(fname_in, "r+")
+    for jobline in fin:
+        cols = jobline.split()
+        src_depth = cols[3]
+        jobname = "src" + src_depth
+        sbatch_file = path2jobs + "/src" + src_depth + ".sh"
+        make_sbatch(jobline, sbatch_file, jobname, NODES_MIN, NODES_MAX, PARTITION, DAYS, HOURS, MEMORY)
+    fin.close()
