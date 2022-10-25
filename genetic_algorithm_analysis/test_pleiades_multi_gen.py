245439bec1d1ed3d64ba881441305162b49071ad
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2022-10-25 15:53:44 +0200

    increasing generations to 90

--- genetic_algorithm_analysis/test_pleiades_multi_gen.py
+++ genetic_algorithm_analysis/test_pleiades_multi_gen.py
@@ -70,7 +70,7 @@ for j in range(nIndividuals):
 #Create n-matrix
 fname_config = 'config_aletsch.txt'
 fname_nmatrix = 'test_nmatrix.h5'
-nGenerations = 60
+nGenerations = 90
 createMatrix(fname_config=fname_config, n_prof_initial=n_prof_initial, z_profile=zprof_0,
              fname_nmatrix=fname_nmatrix, nGenerations = nGenerations)
 #=========================================================================================
