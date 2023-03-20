8cea1c808baf7c3842b496a900502b34c3d7e76c
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2023-03-20 13:06:37 +0100

    fitness function

--- genetic_algorithm_analysis_wd/fitness_function.py
+++ genetic_algorithm_analysis_wd/fitness_function.py
@@ -27,6 +27,7 @@ def inverse_signal_offset(sig_sim, sig_data, mode='abs'):
 def fitness_correlation(sig_sim, sig_data, mode='abs'):
     return inverse_signal_offset(sig_sim, sig_data, mode)
 
+'''
 def fitness_pulse_FT_data(sig_sim, sig_data, mode = 'Correlation'):
     if mode == 'Correlation': # Signal Cross Correlation Method
         sig_multi = abs(sig_sim * sig_data)
@@ -44,6 +45,28 @@ def fitness_pulse_FT_data(sig_sim, sig_data, mode = 'Correlation'):
         sig_multi_sq = sig_multi ** 2
         S = sum(sig_multi_sq)
     return S
+'''
+
+def fitness_pulse_FT_data(sig_sim, sig_data, mode = 'Correlation'):
+    if mode == 'Correlation':
+        nSamples = len(sig_data)
+        s_corr = np.zeros(nSamples)
+        s_auto = np.zeros(nSamples)
+        for i in range(nSamples):
+            s_auto[i] = (abs(sig_sim[i]))*abs(sig_data[i])
+            s_corr[i] = ((sig_sim[i].real + 1j*sig_sim[i].imag) * (sig_data[i].real - 1j*sig_data[i].imag))/s_auto[i]
+        s_corr /= float(nSamples)
+        s = abs(sum(s_corr))**2
+    else:
+        nSamples = len(sig_data)
+        s_corr = np.zeros(nSamples)
+        s_auto = np.zeros(nSamples)
+        for i in range(nSamples):
+            s_auto[i] = (abs(sig_sim[i]))*abs(sig_data[i])
+            s_corr[i] = ((sig_sim[i].real + 1j*sig_sim[i].imag) * (sig_data[i].real - 1j*sig_data[i].imag))/s_auto[i]
+        s_corr /= float(nSamples)
+        s = abs(sum(s_corr))**2
+    return s
 
 def fitness_pulse_FT_data_2(sig_sim, sig_data, mode='Correlation'):
     nData = len(sig_data)
