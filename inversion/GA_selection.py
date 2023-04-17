4b6ccc2dbe7fbf3b7f8e28d90dd683f724125a85
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2023-04-17 15:01:37 +0200

    restore inversion

---
+++ inversion/GA_selection.py
@@ -0,0 +1,163 @@
+import os
+from math import factorial
+import h5py
+import numpy as np
+import matplotlib.pyplot as pl
+from random import randint, uniform
+import random
+from scipy.interpolate import interp1d
+import names
+
+from GA_operators import flat_mutation, gaussian_mutation, clone, cross_breed, cross_breed2
+
+
+class nombre:
+    def __init__(self, name):
+        self.full_name = name
+        self.first_name = name.split()[0]
+        self.last_name = name.split()[1]
+
+
+def prob_from_sdist(S_ind, S_dist):
+    num_lower = float(sum(S < S_ind for S in
+                          S_dist))  # count the number of individual with fitness function lower than the selected_indidivudal
+    num_total = float(len(S_dist))
+    P_ind = num_lower / num_total
+    return P_ind
+
+
+def roulette(prof_list, S_list, nOutput):
+    nIndividuals = len(S_list)
+    ii = 1
+
+    profile_output_list = []
+    S_output_list = []
+    while ii <= nOutput:
+        jj_rand = random.randint(0, nIndividuals - 1)  # Choose Individual randomly
+
+        prof_jj = prof_list[jj_rand]
+        S_jj = S_list[jj_rand]
+        P_jj = prob_from_sdist(S_jj, S_list)
+
+        R = random.uniform(0, 1)
+        if P_jj > R:
+            profile_output_list.append(prof_jj)
+            S_output_list.append(S_jj)
+            ii += 1
+    return profile_output_list, S_output_list
+
+
+def tournament(prof_list, S_list, nOutput):
+    nIndividuals = len(S_list)
+    inds = np.array(S_list).argsort()
+    random.shuffle(inds)
+
+    # Shuffle the list of fitness variables and profile list
+
+    S_list_tournament = S_list[inds]
+    prof_list_tournament = prof_list[inds]
+
+    # Subdivide into groups for tournament
+    nSubgroup = int(float(nIndividuals) / float(nOutput))
+
+    profile_output_list = []
+    S_output_list = []
+
+    # Loop over each subgroup -> Select the best member and append to output list
+    for i in range(nOutput):
+        jj_min = i * nSubgroup
+        jj_max = (i + 1) * nSubgroup - 1
+        S_list_subgroup = S_list_tournament[jj_min:jj_max]
+        prof_list_subgroup = prof_list_tournament[jj_min:jj_max]
+
+        # Select Best Member
+        k_best = np.argmax(S_list_subgroup)
+        prof_best = prof_list_subgroup[k_best]
+        S_best = S_list_subgroup[k_best]
+
+        profile_output_list.append(prof_best)
+        S_output_list.append(S_best)
+    return profile_output_list, S_output_list
+
+def rand_operator(f_cross_over, f_mutation):
+    N = 1000
+    N1 = int(f_cross_over * float(N))
+    N2 = int(f_mutation * float(N)) + N1
+
+    ii_rand = random.randint(0, N - 1)
+    if ii_rand < N1:
+        return 0  # Perform Parent Operation
+    elif ii_rand >= N1 and ii_rand < N2:
+        return 1  # Perform mutation Operation
+    elif ii_rand > N2:
+        return 2  # Perform Immigrant Operation
+
+def selection(prof_list, S_list, prof_list_initial, f_roulette=0.75, f_elite=0.01, f_cross_over=0.75, f_immigrant=0.04,
+              f_mutant=0.25, mutation_thres=0.95):
+    nIndividuals = len(S_list)  # Number of Individuals in the Generation
+    prof_list_initial_l = list(prof_list_initial)
+
+    inds = np.array(S_list).argsort()
+    inds = np.flip(inds)
+    nElite = int(f_elite * float(nIndividuals))
+    S_list_sorted = S_list[inds]
+    prof_list_sorted = prof_list[inds]
+    print('Numbers:\n')
+    print('nIndividuals:', nIndividuals)
+    print('nElites: ', nElite)
+    if nElite > 0:
+        prof_list_elite = prof_list_sorted[:nElite]
+    f_parents = 1 - f_immigrant
+    nParents = int(f_parents * nIndividuals)
+
+    parent_list = []
+
+    nR = int(f_roulette * float(nParents))
+    nT = nParents - nR
+    print('Roulette')
+    roulette_list, S_list_r = roulette(prof_list, S_list, nR)
+    print('Tournament')
+
+    tournament_list, S_list_t = tournament(prof_list, S_list, nT)
+    print()
+    for i in range(nR):
+        parent_list.append(roulette_list[i])
+    for j in range(nT):
+        parent_list.append(tournament_list[j])
+    new_generation = []
+    for i in range(nElite):
+        new_generation.append(prof_list_elite[i])
+    common_list = []
+    ii_common = 1
+    nCommons = nIndividuals - nElite
+    print('Commons')
+    while ii_common <= nCommons:
+        i_operator = rand_operator(f_cross_over, f_mutant)  # TODO: Change this operator
+        if i_operator == 0:  # Cross Breeding
+            print('Cross-Breed')
+            p_list = random.sample(parent_list, 2)
+            p1 = p_list[0]
+            p2 = p_list[1]
+            prof_c = cross_breed2(p1, p2)
+            common_list.append(prof_c)
+            ii_common += 1
+        elif i_operator == 1:
+            print('Mutate')
+
+            # TODO: Add Mutation Different Methods
+            j_rand = np.random.randint(0, nParents - 1, 1)[0]
+            prof_m = flat_mutation(parent_list[j_rand], mutation_thres=mutation_thres)
+            common_list.append(prof_m)
+            ii_common += 1
+
+        elif i_operator == 2:
+            print('Immigrant')
+
+            prof_c = random.sample(prof_list_initial_l, 1)[0]
+            common_list.append(prof_c)
+            ii_common += 1
+
+    for i in range(nCommons):
+        prof_m = common_list[i]
+        new_generation.append(prof_m)
+    return new_generation
---
+++ inversion/makeDepthScan.py
@@ -0,0 +1,113 @@
+import os.path
+import sys
+import numpy as np
+import time
+import datetime
+import h5py
+import configparser
+import datetime
+
+from objective_functions import fitness_function, misfit_function_ij
+
+sys.path.append('../')
+
+import util
+import paraPropPython as ppp
+from receiver import receiver as rx
+from transmitter import tx_signal
+from data import create_sim, create_rxList_from_file, create_transmitter_array, create_hdf_FT
+from data import create_tx_signal, bscan, bscan_rxList, create_hdf_bscan
+
+def depth_scan(fname_config, n_profile, z_profile, fname_out=None):
+    tx_signal = create_tx_signal(fname_config)
+    tx_signal.get_gausspulse()
+    tx_signal.add_gaussian_noise()
+    rxList0 = create_rxList_from_file(fname_config)
+    tx_depths = create_transmitter_array(fname_config)
+
+    nDepths = len(tx_depths)
+    nReceivers = len(rxList0)
+
+    bscan_npy = np.zeros((nDepths, nReceivers, tx_signal.nSamples), dtype='complex')
+
+    print('Running tx scan')
+    for i in range(nDepths):
+        tstart = time.time()
+        sourceDepth = tx_depths[i]
+        print('z = ', sourceDepth)
+
+        rxList = rxList0
+
+        sim = create_sim(fname_config)
+        sim.set_n(nVec=n_profile, zVec=z_profile)  # Set Refractive Index Profile
+        sim.set_dipole_source_profile(tx_signal.frequency, sourceDepth)  # Set Source Profile
+        sim.set_td_source_signal(tx_signal.pulse, tx_signal.dt)  # Set transmitted signal
+        print('solving PE')
+        sim.do_solver(rxList, freqMin=tx_signal.freqMin, freqMax=tx_signal.freqMax)
+        print('complete')
+        tend = time.time()
+        for j in range(nReceivers):
+            rx_j = rxList[j]
+            rx_j.add_gaussian_noise(noise_amplitude=rx_j.noise_amplitude)
+            bscan_npy[i, j] = rx_j.get_signal()
+
+        if i == 0 and fname_out != None:
+            hdf_output = create_hdf_FT(fname=fname_out, sim=sim,
+                                       tx_signal=tx_signal, tx_depths=tx_depths, rxList=rxList)
+
+        duration_s = (tend - tstart)
+        duration = datetime.timedelta(seconds=duration_s)
+        remainder_s = duration_s * (nDepths - (i + 1))
+        remainder = datetime.timedelta(seconds=remainder_s)
+
+        completed = round(float(i + 1) / float(nDepths) * 100, 2)
+        print(completed, ' % completed, duration:', duration)
+        print('remaining steps', nDepths - (i + 1), '\nremaining time:', remainder, '\n')
+        now = datetime.datetime.now()
+        tstamp_now = now.timestamp()
+        end_time = datetime.datetime.fromtimestamp(tstamp_now + remainder_s)
+        print('completion at:', end_time)
+        print('')
+    if fname_out != None:
+        hdf_output.create_dataset('bscan_sig', data=bscan_npy)
+        hdf_output.close()
+
+    return bscan_npy
+
+def depth_scan_from_txt(fname_config, fname_nprofile, fname_out=None):
+    n_profile, z_profile = util.get_profile_from_file(fname_nprofile)
+    bscan_npy = depth_scan(fname_config=fname_config, n_profile=n_profile, z_profile=z_profile, fname_out=fname_out)
+    return bscan_npy
+
+def depth_scan_from_hdf(fname_config, fname_n_matrix, ii_generation, jj_select, fname_pseudo = None, fname_out=None):
+    n_matrix_hdf = h5py.File(fname_n_matrix,'r') #The matrix holding n_profiles
+    fname_n_matrix_npy = fname_n_matrix[:-3] + '.npy'
+    fname_misfit_npy = fname_n_matrix[:-3] + '_misfit.npy'
+    n_profile_matrix = np.array(n_matrix_hdf.get('n_profile_matrix'))
+    n_profile_ij = n_profile_matrix[ii_generation,jj_select] #The Individual (n profile) contains genes (n values per z)
+    z_profile_ij = np.array(n_matrix_hdf.get('z_profile'))
+    n_matrix_hdf.close()
+
+    bscan_npy = depth_scan(fname_config=fname_config, n_profile=n_profile_ij, z_profile=z_profile_ij, fname_out=fname_out)
+
+    if fname_pseudo != None:
+        bscan_pseudo = bscan_rxList()
+        bscan_pseudo.load_sim(fname_pseudo)
+        tspace = bscan_pseudo.tspace
+        nDepths = len(bscan_npy)
+        nReceivers = len(bscan_npy[0])
+        misfit_arr = np.load(fname_misfit_npy,'r+')
+        misfit_total = 0
+        for i in range(nDepths):
+            for j in range(nReceivers):
+                m_ij = misfit_function_ij(sig_data=bscan_pseudo.get_ascan(i,j),
+                                       sig_sim=bscan_npy[i,j],
+                                       tspace=tspace)
+                misfit_arr[ii_generation, jj_select, i, j] = m_ij
+                misfit_total += m_ij
+        misfit_total /= (2. * float(nDepths)*float(nReceivers))
+        #print('Misfit total:', misfit_total, misfit_arr[ii_generation, jj_select])
+        S_corr = 1/misfit_total
+        print('S=', S_corr)
+        S_arr_out = np.load(fname_n_matrix_npy, 'r+')
+        S_arr_out[ii_generation, jj_select] = S_corr #WRITE TO NPY FILE
