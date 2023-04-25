e7f2cf4085ab01f0fcb53a70fb55f09cf6fbbdb1
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2023-04-25 15:12:29 +0200

    plot_S

--- inversion/plot_Sarr.py
+++ inversion/plot_Sarr.py
@@ -19,7 +19,6 @@ from data import create_sim, create_rxList_from_file, create_transmitter_array, 
 from data import create_tx_signal, bscan, bscan_rxList, create_hdf_bscan
 
 fname_nmatrix = sys.argv[1]
-
 nmatrix_hdf = h5py.File(fname_nmatrix, 'r')
 n_profile_matrix = np.array(nmatrix_hdf['n_profile_matrix'])
 S_arr = np.array(nmatrix_hdf['S_arr'])
@@ -44,6 +43,18 @@ for i in range(nGenerations):
         S_med_list.append(S_median)
         S_mean_list.append(S_mean)
         gens.append(i)
+
+parent_dir = os.path.dirname(fname_nmatrix)
+cwd = str(os.getcwd())
+print(cwd)
+import subprocess
+output = str(subprocess.check_output("ls " + parent_dir + "/*pseudo*", shell=True))[2:-3:]
+print(output)
+pseudo_data = cwd + '/' + output
+print(pseudo_data)
+parent_size = len(parent_dir) + 1
+job_label = fname_nmatrix[parent_size:-3]
+
 S_max_list = np.array(S_max_list)
 S_mean_list = np.array(S_mean_list)
 S_med_list = np.array(S_med_list)
@@ -54,10 +65,10 @@ ax.plot(gens, S_med_list, c='g',label='Median')
 ax.plot(gens, S_mean_list,c='r',label='Mean')
 ax.set_xlabel('Generation')
 ax.legend()
-ax.set_ylabel('Fitness Score $S = (\sum_{ij }m_{ij})^{-1} $')
+ax.set_ylabel('Fitness Score $S = (\sum_{ij }\chi_{ij})^{-1} $')
 ax.grid()
-fig.savefig('S_score.png')
-pl.show()
+fig.savefig(parent_dir + '/' + job_label + '_S_score.png')
+pl.close(fig)
 
 fig = pl.figure(figsize=(8,5),dpi=120)
 ax = fig.add_subplot(111)
@@ -66,7 +77,118 @@ ax.plot(gens, 1/S_med_list, c='g',label='Median')
 ax.plot(gens, 1/S_mean_list,c='r',label='Mean')
 ax.set_xlabel('Generation')
 ax.legend()
-ax.set_ylabel('Global Misfit Score $\sum_{ij} m_{ij}$')
+ax.set_ylabel('Global Misfit Score $\sum_{ij} \chi_{ij}$')
 ax.grid()
-fig.savefig('M_score.png')
+fig.savefig(parent_dir + '/' + job_label + '_M_score.png')
+pl.close(fig)
+
+all_scores = []
+all_misfit = []
+all_gens = []
+
+
+for i in range(nGenerations-1):
+    S_list = S_arr[i]
+    nIndividuals = len(S_list)
+    for j in range(nIndividuals):
+        S_ij = S_list[j]
+        M_ij = 1/S_ij
+        if S_ij != 0:
+            all_scores.append(S_ij)
+            all_misfit.append(M_ij)
+            all_gens.append(i)
+#print(all_scores)
+all_scores = np.array(all_scores)
+all_misfit = np.array(all_misfit)
+all_gens = np.array(all_gens)
+N = len(all_scores) / float((nGenerations-1)*nIndividuals)
+print(N)
+#print(nGenerations_finished)
+
+#H, xedges, yedges = np.histogram2d(all_gens, all_scores, bins=[nGenerations, nGenerations])
+import matplotlib as mpl
+
+'''
+fig = pl.figure(figsize=(8,5),dpi=120)
+ax = fig.add_subplot(111)
+#ax.hist2d(all_gens, all_scores, bins=[nGenerations, nGenerations], norm=mpl.colors.LogNorm())
+ax.hist2d(all_gens, all_scores, bins=[nGenerations, nGenerations])
+
+ax.plot(gens, S_max_list,c='b',label='Max')
+ax.plot(gens, S_med_list, c='g',label='Median')
+ax.plot(gens, S_mean_list,c='r',label='Mean')
+ax.legend()
+ax.set_xlabel('Generation')
+ax.set_ylabel('Score')
 pl.show()
+'''
+
+fig = pl.figure(figsize=(8,5),dpi=120)
+ax = fig.add_subplot(111)
+h = ax.hist2d(all_gens, all_misfit, bins=[nGenerations, nGenerations], range=[[0, nGenerations-1],[200, 300]])
+ax.plot(gens, 1/S_max_list,c='b',label='Max')
+ax.plot(gens, 1/S_med_list, c='g',label='Median')
+ax.plot(gens, 1/S_mean_list,c='r',label='Mean')
+fig.colorbar(h[3], ax=ax)
+ax.legend()
+ax.set_xlabel('Generation')
+ax.set_ylabel('Misfit')
+pl.close(fig)
+
+'''
+fig = pl.figure(figsize=(8,5),dpi=120)
+ax = fig.add_subplot(111)
+ax.hist2d(all_gens, all_misfit, bins=[nGenerations, nGenerations], range=[[0, nGenerations-1],[200, 600]],
+          norm=mpl.colors.LogNorm())
+ax.plot(gens, 1/S_max_list,c='b',label='Max')
+ax.plot(gens, 1/S_med_list, c='g',label='Median')
+ax.plot(gens, 1/S_mean_list,c='r',label='Mean')
+ax.legend()
+ax.set_xlabel('Generation')
+ax.set_ylabel('Misfit')
+pl.show()
+'''
+i_select = 1
+for i_select in range(nGenerations):
+    n_genes_gen = genes_arr[i_select]
+    n_prof_list = []
+    z_list = []
+    nDepths = len(n_genes_gen[i_select])
+
+    bscan_pseudo = bscan_rxList()
+    print(type(pseudo_data))
+    bscan_pseudo.load_sim(pseudo_data)
+    n_prof_pseudo = bscan_pseudo.n_profile
+    z_prof_pseudo = bscan_pseudo.z_profile
+    for i in range(len(n_genes_gen)):
+        genes_ind = n_genes_gen[i]
+        for j in range(nDepths):
+            n_prof_list.append(genes_ind[j])
+            z_list.append(-z_genes[j])
+    fig = pl.figure(figsize=(5,8))
+    ax = fig.add_subplot(111)
+    h = ax.hist2d(n_prof_list, z_list, bins=[40, 2*nDepths], range=[[1.2,1.8],[-15,-1.5]],
+                  cmap='viridis_r',vmin=1, vmax=80,norm=mpl.colors.LogNorm())
+    #ax.scatter(n_prof_list, z_list, c='b')
+
+    fig.colorbar(h[3], ax=ax)
+    ax.set_title('Gen = ' + str(i_select))
+    ax.plot(n_prof_pseudo, -1*z_prof_pseudo, c='c')
+    ax.set_label('Ref Index')
+    ax.set_ylabel('Depth Z[m]')
+    ax.set_ylim(-16,0)
+    fig.savefig(parent_dir + '/' + 'nprof_range' + str(i_select).zfill(3) + '.png')
+    pl.close(fig)
+
+    fig = pl.figure(figsize=(5, 8))
+    ax = fig.add_subplot(111)
+    ax.scatter(n_prof_list, z_list, c='b',s=0.5)
+    ax.set_title('Gen = ' + str(i_select))
+    ax.plot(n_prof_pseudo, -1 * z_prof_pseudo, c='k')
+    ax.set_label('Ref Index')
+    ax.set_ylabel('Depth Z[m]')
+    ax.set_ylim(-16, 0)
+    ax.grid()
+    ax.set_xlim(1.2,1.8)
+    fig.savefig(parent_dir + '/' + 'nprof_scatter' + str(i_select).zfill(3) + '.png')
+    pl.close(fig)
