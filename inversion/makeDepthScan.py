af0001fe73ba57f4af93daf5365eca7a292aa602
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2023-04-27 14:51:38 +0200

    Adding Changes to Function

--- inversion/makeDepthScan.py
+++ inversion/makeDepthScan.py
@@ -18,6 +18,27 @@ from transmitter import tx_signal
 from data import create_sim, create_rxList_from_file, create_transmitter_array, create_hdf_FT
 from data import create_tx_signal, bscan, bscan_rxList, create_hdf_bscan
 
+
+def ascan(fname_config, n_profile, z_profile, z_tx, x_rx, z_rx): #TODO: Add Output File
+    tx_signal = create_tx_signal(fname_config)
+    tx_signal.get_gausspulse()
+    tx_signal.add_gaussian_noise()
+    rxList = []
+    rx_1 = rx(x=x_rx, z=z_rx)
+    sourceDepth = z_tx
+    rxList.append(rx_1)
+
+    sim = create_sim(fname_config)
+    sim.set_n(nVec=n_profile, zVec=z_profile)  # Set Refractive Index Profile
+    sim.set_dipole_source_profile(tx_signal.frequency, sourceDepth)  # Set Source Profile
+    sim.set_td_source_signal(tx_signal.pulse, tx_signal.dt)  # Set transmitted signal
+    print('solving PE')
+    sim.do_solver(rxList, freqMin=tx_signal.freqMin, freqMax=tx_signal.freqMax)
+
+    rx_out = rxList[0]
+    signal_out = rx_out.get_signal()
+    return signal_out
+
 def depth_scan(fname_config, n_profile, z_profile, fname_out=None):
     tx_signal = create_tx_signal(fname_config)
     tx_signal.get_gausspulse()
@@ -88,6 +109,13 @@ def depth_scan_from_hdf(fname_config, fname_n_matrix, ii_generation, jj_select, 
     z_profile_ij = np.array(n_matrix_hdf.get('z_profile'))
     n_matrix_hdf.close()
 
+    config = configparser.ConfigParser()
+    config.read(fname_config)
+
+    # Set up Simulation Output Files
+    print('set up output files')
+    fitness_mode = config['INPUT']['Fitness']
+
     bscan_npy = depth_scan(fname_config=fname_config, n_profile=n_profile_ij, z_profile=z_profile_ij, fname_out=fname_out)
 
     if fname_pseudo != None:
@@ -103,7 +131,7 @@ def depth_scan_from_hdf(fname_config, fname_n_matrix, ii_generation, jj_select, 
             for j in range(nReceivers):
                 m_ij = misfit_function_ij(sig_data=bscan_pseudo.get_ascan(i,j),
                                        sig_sim=bscan_npy[i,j],
-                                       tspace=tspace)
+                                       tspace=tspace, mode=fitness_mode)
                 if os.path.isfile(fname_misfit_npy) == True:
                     misfit_arr[ii_generation, jj_select, i, j] = m_ij
                 misfit_total += m_ij
@@ -113,3 +141,29 @@ def depth_scan_from_hdf(fname_config, fname_n_matrix, ii_generation, jj_select, 
         print('S=', S_corr)
         S_arr_out = np.load(fname_n_matrix_npy, 'r+')
         S_arr_out[ii_generation, jj_select] = S_corr #WRITE TO NPY FILE
+
+'''
+def depth_scan_from_hdf_data(fname_config, fname_n_matrix, ii_generation, jj_select, fname_data = None, fname_out=None):
+    n_matrix_hdf = h5py.File(fname_n_matrix,'r') #The matrix holding n_profiles
+    fname_n_matrix_npy = fname_n_matrix[:-3] + '.npy'
+    fname_misfit_npy = fname_n_matrix[:-3] + '_misfit.npy'
+    n_profile_matrix = np.array(n_matrix_hdf.get('n_profile_matrix'))
+    n_profile_ij = n_profile_matrix[ii_generation,jj_select] #The Individual (n profile) contains genes (n values per z)
+    z_profile_ij = np.array(n_matrix_hdf.get('z_profile'))
+    n_matrix_hdf.close()
+
+    bscan_npy = depth_scan(fname_config=fname_config, n_profile=n_profile_ij, z_profile=z_profile_ij, fname_out=fname_out)
+    #TODO Finish
+'''
+
+def ascan_from_hdf(fname_config, fname_n_matrix, ii_generation, jj_select, z_tx, x_rx, z_rx):
+    n_matrix_hdf = h5py.File(fname_n_matrix,'r') #The matrix holding n_profiles
+    fname_n_matrix_npy = fname_n_matrix[:-3] + '.npy'
+    fname_misfit_npy = fname_n_matrix[:-3] + '_misfit.npy'
+    n_profile_matrix = np.array(n_matrix_hdf.get('n_profile_matrix'))
+    n_profile_ij = n_profile_matrix[ii_generation,jj_select] #The Individual (n profile) contains genes (n values per z)
+    z_profile_ij = np.array(n_matrix_hdf.get('z_profile'))
+    n_matrix_hdf.close()
+
+    return ascan(fname_config=fname_config, n_profile = n_profile_ij, z_profile = z_profile_ij,
+                 z_tx=z_tx, x_rx=x_rx, z_rx=z_rx)
--- inversion/objective_functions.py
+++ inversion/objective_functions.py
@@ -3,6 +3,10 @@ import sys
 from scipy.signal import correlate as cc
 sys.path.append('../')
 import util
+import peakutils as pku
+import scipy
+def check_symmetric(a, tol=1e-10):
+    return np.all((a - np.flip(a)) < tol)
 
 def misfit_function_ij2(sig_data, sig_sim, tspace, tmin=None, tmax=None, mode='Waveform'):
     dt = abs(tspace[1] - tspace[0])
@@ -57,6 +61,54 @@ def misfit_function_ij(sig_data, sig_sim, tspace, tmin=None, tmax=None, mode='En
             envelope_data_i = np.sqrt(sig_data[i].real**2 + sig_data[i].imag**2)
             chi_local_i = ((np.log(envelope_data_i/envelope_sim_i))**2 ) * dt
             chi_local += chi_local_i
+    elif mode == 'Correlation':
+        chi_local = 0
+        j = 0
+        sig_cross_correl = abs(cc(sig_sim, sig_data))
+        sig_auto = abs(cc(sig_data, sig_data))
+        t_space_lag = np.linspace(-max(tspace), max(tspace), len(sig_cross_correl))
+        indexes = pku.indexes(sig_cross_correl, thres=0.05)
+        indexes2 = pku.indexes(sig_auto, thres=0.1)
+        nPeaks = len(indexes)
+        '''
+        print(len(indexes), len(indexes2))
+        i_mid = np.argmin(abs(t_space_lag))
+        print(len(sig_cross_correl), max(t_space_lag), min(t_space_lag), t_space_lag[i_mid])
+        print(len(t_space_lag[i_mid+1:len(t_space_lag)]), len(t_space_lag[0:i_mid]))
+        t_positive = t_space_lag[i_mid+1:len(t_space_lag)]
+        t_negative = np.flip(t_space_lag[0:i_mid])
+        sig_cross_pos = sig_cross_correl[i_mid+1:len(t_space_lag)]
+        sig_cross_neg = np.flip(sig_cross_correl[0:i_mid])
+        sig_cross_delta = abs(sig_cross_pos / sig_cross_neg)
+        peaks_3 = pku.indexes(sig_cross_delta, thres=0.05)
+        nPeaks_3 = len(peaks_3)
+        '''
+        print('Check Array Symmetry')
+        #array_symmetry = scipy.linalg.issymmetric(sig_cross_correl)
+        #if array_symmetry == False:
+        if check_symmetric(sig_cross_correl) == False:
+            print('False')
+            for j in range(nPeaks):
+                k_peaks = indexes[j]
+                amp_off = (np.log(sig_cross_correl[k_peaks]/max(sig_auto)))**2
+                t_offset = abs(t_space_lag[k_peaks]/dt)
+                weight = sig_cross_correl[k_peaks]/sig_auto[k_peaks]
+
+                chi_local += t_offset * weight
+                '''
+                k_peak = peaks_3[j]
+                t_offset = t_positive[k_peak]
+                weight = sig_cross_delta[k_peak]
+                chi_local += t_offset * weight
+                #chi_local += amp_off
+                '''
+            #i_mid = int((len(t_space_lag) - 1) / 2)
+            #chi_local += (sig_cross_correl[i_mid] / sig_auto[i_mid]) * dt
+            chi_local += 1
+        else:
+            print('True')
+            i_mid = int((len(t_space_lag) - 1)/2)
+            chi_local = (np.log(sig_cross_correl[i_mid]/sig_auto[i_mid]))**2
     return chi_local
 
 def misfit_function_ij0(sig_data, sig_sim, tspace, tmin=None, tmax=None, mode='Waveform'):
@@ -113,11 +165,28 @@ def misfit_function_ij0(sig_data, sig_sim, tspace, tmin=None, tmax=None, mode='W
         chi_local = 0
         j = 0
         for i in range(ii_min,ii_max):
-            chi_local_i = abs(sig_data[i] - sig_sim[i])**2 * dt
+            chi_local_i = abs(abs(sig_data[i]) - abs(sig_sim[i]))**2 * dt
             chi_local += chi_local_i
             chi_out[j] = chi_local_i
             chi_sum[j] = chi_local
             j += 1
+    '''
+    elif mode == 'Correlation':
+        chi_local = 0
+        j = 0
+        sig_cross_correl = abs(cc(sig_sim, sig_data))
+        t_space_lag = np.linspace(-max(tspace), max(tspace), len(sig_cross_correl))
+        k_cut = util.findNearest(t_space_lag, 0)
+        t_space_lag = t_space_lag[k_cut:]
+        sig_cross_correl = sig_cross_correl[k_cut:]
+        k_max = np.argmax(sig_cross_correl)
+        t_max_cc = t_space_lag[k_max]
+        for i in range(ii_min, ii_max):
+            chi_local = abs(sig_data[i].real * sig_sim[i].real)**2 * dt
+            chi_local
+    '''
+
+
     return chi_local, chi_out, t_out, chi_sum
 
 
