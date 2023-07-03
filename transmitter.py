e2598a3c7336acdb0e0046a6cecc7939d114a2c1
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2023-07-03 19:20:49 +0200

    adding tx functions

--- transmitter.py
+++ transmitter.py
@@ -4,6 +4,7 @@ import numpy as np
 from inspect import signature
 from scipy.interpolate import interp1d
 from scipy import signal
+from scipy.signal.windows import tukey
 # A. Kyriacou
 
 '''
@@ -46,6 +47,13 @@ class tx_signal:
             self.freqMin = freqMin
         '''
 
+    def set_impulse(self):
+        jj = util.findNearest(self.tspace, self.t_centre)
+        self.pulse = np.zeros(self.nSamples)
+        self.pulse[jj] = self.amplitude
+        self.spectrum = np.fft.fft(self.pulse)
+        return self.pulse
+
     def get_gausspulse_complex(self, suppression = -60):
         frac_bandwidth = self.bandwidth / self.frequency
         pulse_r = self.amplitude * signal.gausspulse(self.tspace - self.t_centre, fc=self.frequency, bw=frac_bandwidth, bwr=suppression)
@@ -70,7 +78,15 @@ class tx_signal:
 
     def get_spectrum(self): #NOTE: pulse must be defined before
         return self.spectrum
-    
+
+    def apply_bandpass(self, fmin = self.freqMin, fmax = self.freqMax, order=3):
+        self.pulse = util.butterBandpassFilter(data=self.pulse,
+                                               lowcut=fmin,
+                                               highcut=fmax,
+                                               fs=1/self.dt,
+                                               order=order)
+        return self.pulse
+
     def set_pulse(self, pulse_data, tspace_pulse):
         self.pulse = pulse_data
         self.spectrum = np.fft.fft(self.pulse)
@@ -89,16 +105,32 @@ class tx_signal:
         nFreq = len(freq_shift)
         IR_fmin = min(IR_freq)
         IR_fmax = max(IR_freq)
-        self.IR = np.ones(nFreq)
-        for i in range(nFreq):
-            freq_i = self.freq_space[i]
-            if freq_i < IR_fmin:
-                self.IR[i] = 0
-            elif freq_i > IR_fmax:
-                self.IR[i] = 0
-            elif freq_i <= IR_fmax and freq_i >= IR_fmin:
-                jj = util.findNearest(IR_freq, freq_i)
-                self.IR[i] = IR[jj]
+        nMid = util.findNearest(freq_shift, 0)
+
+        freq_shift_positive = freq_shift[nMid:]
+        freq_shift_negative = -1* np.flip(freq_shift[:nMid])
+        nFreq_pos = len(freq_shift_positive)
+        nFreq_neg = len(freq_shift_positive)
+        self.IR = np.zeros(nFreq)
+        self.IR_positive = np.zeros(nFreq_pos)
+        self.IR_negative = np.zeros(nFreq_neg)
+        for i in range(nFreq_pos):
+            freq_plus = freq_shift_positive[i]
+            j_neg = util.findNearest(freq_shift_negative, freq_plus)
+            freq_neg = freq_shift_negative[j_neg]
+            if freq_plus < IR_fmin:
+                self.IR_positive[i] = 0
+                self.IR_negative[j_neg] = 0
+            elif freq_plus > IR_fmax:
+                self.IR_positive[i] = 0
+                self.IR_negative[j_neg] = 0
+            elif freq_plus <= IR_fmax and freq_plus >= IR_fmin:
+                print('reset')
+                k_pos = util.findNearest(IR_freq, freq_plus)
+                self.IR_positive[i] = IR[k_pos]
+                self.IR_negative[j_neg] = IR[k_pos]
+        self.IR[nMid:] = self.IR_positive
+        self.IR[:nMid] = np.flip(self.IR_negative)
 
         spectrum_shift *= self.IR
         self.spectrum = np.fft.ifftshift(spectrum_shift)
@@ -106,6 +138,73 @@ class tx_signal:
 
         return self.pulse
 
+    def set_psuedo_fmcw(self, fmin, fmax, IR, IR_freq):
+        #Set Impulse
+        jj = util.findNearest(self.tspace, self.t_centre)
+        self.pulse = np.zeros(self.nSamples)
+        self.pulse[jj] = self.amplitude
+        self.spectrum = np.fft.fft(self.pulse)
+
+        #Convert to FD
+        spectrum_shift = np.fft.fftshift(self.spectrum)
+        freq_space = np.fft.fftfreq(self.nSamples, self.dt)
+        freq_space = np.fft.fftshift(freq_space)
+
+        #Convert to POS AND NEG
+        nMid = util.findNearest(freq_space, 0)
+        freq_shift_positive = freq_space[nMid:]
+        freq_shift_negative = -1* np.flip(freq_space[:nMid])
+        spectrum_positive = spectrum_shift[nMid:]
+        spectrum_negative = np.flip(spectrum_shift[:nMid])
+
+        #CUT FD to Limits of Bandwidth
+        ii_min_plus = util.findNearest(freq_shift_positive, fmin)
+        ii_max_plus = util.findNearest(freq_shift_positive, fmax)
+        nDelta_plus = ii_max_plus - ii_min_plus
+
+        ii_min_minus = util.findNearest(freq_shift_negative, fmin)
+        ii_max_minus = util.findNearest(freq_shift_negative, fmax)
+        nDelta_minus = ii_max_minus - ii_min_minus
+
+        spectrum_positive[:ii_min_plus] = 0
+        spectrum_positive[ii_max_plus:] = 0
+        #spectrum_positive[ii_min_plus:ii_max_plus] *= np.blackman(nDelta_plus) #
+        spectrum_positive[ii_min_plus:ii_max_plus] *= tukey(nDelta_plus)
+        spectrum_negative[:ii_min_minus] = 0
+        spectrum_negative[ii_max_minus:] = 0
+        #spectrum_negative[ii_min_minus:ii_max_minus] *= np.blackman(nDelta_minus) #
+        spectrum_negative[ii_min_minus:ii_max_minus] *=tukey(nDelta_minus)
+        nFreq_pos = len(freq_shift_positive)
+        nFreq_neg = len(freq_shift_positive)
+        nFreq = len(freq_space)
+        IR_fmin = min(IR_freq)
+        IR_fmax = max(IR_freq)
+        self.IR = np.zeros(nFreq)
+        self.IR_positive = np.zeros(nFreq_pos)
+        self.IR_negative = np.zeros(nFreq_neg)
+        for i in range(nFreq_pos):
+            freq_plus = freq_shift_positive[i]
+            j_neg = util.findNearest(freq_shift_negative, freq_plus)
+            freq_neg = freq_shift_negative[j_neg]
+            if freq_plus < IR_fmin:
+                self.IR_positive[i] = 0
+                self.IR_negative[j_neg] = 0
+            elif freq_plus > IR_fmax:
+                self.IR_positive[i] = 0
+                self.IR_negative[j_neg] = 0
+            elif freq_plus <= IR_fmax and freq_plus >= IR_fmin:
+                print('reset')
+                k_pos = util.findNearest(IR_freq, freq_plus)
+                self.IR_positive[i] = IR[k_pos]
+                self.IR_negative[j_neg] = IR[k_pos]
+        self.IR[nMid:] = self.IR_positive
+        self.IR[:nMid] = np.flip(self.IR_negative)
+
+        spectrum_shift *= self.IR
+        self.spectrum = np.fft.ifftshift(spectrum_shift)
+        self.pulse = np.fft.ifft(self.spectrum)
+
+        return self.pulse
 
     def add_gaussian_noise(self):
         nSamples = len(self.spectrum)
