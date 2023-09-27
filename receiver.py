d16efb58270af3e41b2196eed6eaba95e0fc9747
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2023-09-27 12:01:38 +0200

    do_impulse_response

--- receiver.py
+++ receiver.py
@@ -57,7 +57,45 @@ class receiver:
         self.spectrum_plus = np.zeros(self.nFreq, dtype='complex')
         self.spectrum_minus = np.zeros(self.nFreq, dtype='complex')
         self.time = np.arange(0, dt * self.nFreq, dt)
-        self.IR = np.ones(self.nFreq)
+        self.nSamples = len(self.time)
+        self.dt = dt
+        self.freq_space = np.fft.fftfreq(self.nSamples, dt)
+
+        self.IR = np.ones(self.nSamples)
+        '''
+        spectrum_shift = np.fft.fftshift(self.spectrum)
+        freq_shift = np.fft.fftshift(self.freq_space)
+        nFreq = len(freq_shift)
+        IR_fmin = min(self.IR_freq)
+        IR_fmax = max(self.IR_freq)
+        nMid = util.findNearest(freq_shift, 0)
+
+        freq_shift_positive = freq_shift[nMid:]
+        freq_shift_negative = -1* np.flip(freq_shift[:nMid])
+
+        nFreq_pos = len(freq_shift_positive)
+        nFreq_neg = len(freq_shift_positive)
+        self.IR = np.zeros(nFreq)
+        self.IR_positive = np.zeros(nFreq_pos)
+        self.IR_negative = np.zeros(nFreq_neg)
+        for i in range(nFreq_pos):
+            freq_plus = freq_shift_positive[i]
+            j_neg = util.findNearest(freq_shift_negative, freq_plus)
+            freq_neg = freq_shift_negative[j_neg]
+            if freq_plus < IR_fmin:
+                self.IR_positive[i] = 0
+                self.IR_negative[j_neg] = 0
+            elif freq_plus > IR_fmax:
+                self.IR_positive[i] = 0
+                self.IR_negative[j_neg] = 0
+            elif freq_plus <= IR_fmax and freq_plus >= IR_fmin:
+                print('reset')
+                k_pos = util.findNearest(self.IR_freq, freq_plus)
+                self.IR_positive[i] = self.IR_data[k_pos]
+                self.IR_negative[j_neg] = self.IR_data[k_pos]
+        self.IR[nMid:] = self.IR_positive
+        self.IR[:nMid] = np.flip(self.IR_negative)
+
         if len(self.IR_data) > 0 and len(self.IR_freq) > 0:
             IR_fmin = min(self.IR_freq)
             IR_fmax = max(self.IR_freq)
@@ -75,6 +113,7 @@ class receiver:
         elif len(self.IR_freq) > 0 and len(self.IR_data) == 0:
             print('warning! you set IR frequnecy values but not the IR data -> IR not implemented')
         #TODO -> Add elif and else to self.IR_data
+        '''
     def add_spectrum_component(self, f, A):
         """
         adds the contribution of a frequency to the received signal spectrum
@@ -177,18 +216,54 @@ class receiver:
         """
         return self.time
 
-    def do_impulse_response(self, h):
+    def do_impulse_response(self, IR_freq, IR_data):
         '''
         Applies Convolution of received amplitude and antenna's impulse response function
         -> Simply applies
         '''
-        self.impulse_response = h
-        self.spectrum *= self.impulse_response
-        self.spectrum_plus *= self.impulse_response
-        self.spectrum_minus *= self.impulse_response
+        self.IR = np.ones(self.nSamples)
+        self.IR_freq = IR_freq
+        self.IR_data = IR_data
+
+        spectrum_shift = np.fft.fftshift(self.spectrum)
+        freq_shift = np.fft.fftshift(self.freq_space)
+        nFreq = len(freq_shift)
+        IR_fmin = min(self.IR_freq)
+        IR_fmax = max(self.IR_freq)
+        nMid = util.findNearest(freq_shift, 0)
+
+        freq_shift_positive = freq_shift[nMid:]
+        freq_shift_negative = -1 * np.flip(freq_shift[:nMid])
+
+        nFreq_pos = len(freq_shift_positive)
+        nFreq_neg = len(freq_shift_positive)
+        self.IR = np.zeros(nFreq)
+        self.IR_positive = np.zeros(nFreq_pos)
+        self.IR_negative = np.zeros(nFreq_neg)
+        for i in range(nFreq_pos):
+            freq_plus = freq_shift_positive[i]
+            j_neg = util.findNearest(freq_shift_negative, freq_plus)
+            freq_neg = freq_shift_negative[j_neg]
+            if freq_plus < IR_fmin:
+                self.IR_positive[i] = 0
+                self.IR_negative[j_neg] = 0
+            elif freq_plus > IR_fmax:
+                self.IR_positive[i] = 0
+                self.IR_negative[j_neg] = 0
+            elif freq_plus <= IR_fmax and freq_plus >= IR_fmin:
+                print('reset')
+                k_pos = util.findNearest(self.IR_freq, freq_plus)
+                self.IR_positive[i] = self.IR_data[k_pos]
+                self.IR_negative[j_neg] = self.IR_data[k_pos]
+        self.IR[nMid:] = self.IR_positive
+        self.IR[:nMid] = np.flip(self.IR_negative)
+
+        self.spectrum *= self.IR
+        self.spectrum_plus *= self.IR
+        self.spectrum_minus *= self.IR
 
     def get_impulse_response(self):
-        return self.impulse_response
+        return self.IR
     '''
     def add_gaussian_noise(self):
         nSamples = len(self.spectrum)
