d560d89d524732109a4a3173f80d22f985e63ea5
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2023-10-30 22:40:39 +1030

    adding ascan

---
+++ firn_variance_study/ascan_multi.py
@@ -0,0 +1,86 @@
+import os.path
+import sys
+import numpy as np
+import time
+import datetime
+import h5py
+import configparser
+from numpy import exp, log
+import matplotlib.pyplot as pl
+from ku_scripting import *
+
+
+sys.path.append('../')
+from paraPropPython import paraProp as ppp
+import receiver as rx
+from transmitter import tx_signal
+from data import create_sim, create_rxList_from_file, create_tx_signal, create_transmitter_array
+from data import create_hdf_bscan, bscan_rxList, create_hdf_FT
+from data import create_ascan_hdf, ascan
+import util
+
+def create_spectrum(fname_config, nprof_data, zprof_data,  fname_output_npy, fname_output_h5):
+    tx_signal_out = create_tx_signal(fname_config)
+    rxList = create_rxList_from_file(fname_config)
+    txList = create_transmitter_array(fname_config)
+
+    nSamples = tx_signal_out.nSamples
+    nRX = len(rxList)
+    nTX = len(txList)
+
+    #ascan_npy = np.zeros(nTX, nRX, nSamples) #TODO Create Memmp
+    spectrum_npy = util.create_memmap(fname_output_npy,
+                                   (nTX, nRX, nSamples))
+    hdf_ascan = create_ascan_hdf(fname_config=fname_config,
+                                 nprof_data=nprof_data,
+                                 zprof_data=zprof_data,
+                                 fname_output=fname_output_h5)
+    return spectrum_npy
+
+def run_scan_impulse(fname_config, fname_output, nprofile_data, zprofile_data, ii_freq, jj_tx):
+    tx_signal_out = create_tx_signal(fname_config)
+    rxList = create_rxList_from_file(fname_config)
+    txList = create_transmitter_array(fname_config)
+
+    nSamples = tx_signal_out.nSamples
+    nRX = len(rxList)
+    nTX = len(txList)
+
+    tx_signal.set_impulse()
+    f_centre = tx_signal.frequency
+    bandwidth = tx_signal.bandwidth
+    fmin = f_centre - bandwidth/2
+    fmax = f_centre + bandwidth/2
+    tx_signal.apply_bandpass(fmin=fmin, fmax=fmax)
+    tx_signal.add_gaussian_noise()
+
+    spectrum = tx_signal.get_spectrum()
+    freq_space = tx_signal.get_freq_space()
+
+    amp_select = spectrum[ii_freq]
+    freq_select = freq_space[ii_freq]
+    z_tx = txList[jj_tx]
+
+    sim = create_sim(fname_config)
+    sim.set_n(nVec=nprofile_data, zVec=zprofile_data)  # Set Refractive Index Profile
+    sim.set_dipole_source_profile(centerFreq=freq_select, depth=z_tx, A=amp_select)  # Set Source Profile
+    sim.set_cw_source_signal(freq_select)
+    sim.do_solver()
+
+    spectrum_npy = np.load(fname_output, 'w+')
+    for kk_rx in range(nRX):
+        rx_kk = rxList[kk_rx]
+        x_rx = rx_kk.x
+        z_rx = rx_kk.z
+        spectrum_npy[jj_tx, kk_rx, ii_freq] = sim.get_field(x0=x_rx,z0=z_rx)
+
+def save_spectrum(fname_output_npy, fname_output_hdf):
+    spectrum_npy = np.load(fname_output_npy)
+    ascan_out = ascan()
+    ascan_out.load_from_hdf(fname_output_hdf)
+    ascan_out.load_spectrum(spectrum_npy)
+def get_dates(fname_in):
+    with  h5py.File(fname_in, 'r') as input_hdf:
+        data_matrix = np.array(input_hdf['density'])
+    date_arr = data_matrix[1:,0]
+    return date_arr
---
+++ firn_variance_study/genSim_ascan.py
@@ -0,0 +1,83 @@
+import os.path
+import sys
+import numpy as np
+import time
+import datetime
+import h5py
+import configparser
+from numpy import exp, log
+import matplotlib.pyplot as pl
+from ku_scripting import *
+
+
+sys.path.append('../')
+from paraPropPython import paraProp as ppp
+import receiver as rx
+from transmitter import tx_signal
+from data import create_sim, create_rxList_from_file, create_tx_signal, create_transmitter_array
+from data import create_hdf_bscan, bscan_rxList, create_hdf_FT
+import util
+
+from ascan_multi import *
+
+
+fname_config = 'config_ICRC_summit_km_ku.txt'
+fname_nprofile = 'nProf_CFM_deep.h5'
+fname_CFM = 'CFMresults.hdf5'
+
+date_arr = get_dates(fname_CFM)
+
+dir_sim = 'CFM_files_ku'
+dir_sim_path = dir_sim + '/'
+
+start_year = 2011
+end_year = int(date_arr[-1]) + 1
+year_list = np.arange(start_year, end_year, 1)
+year_id_list = []
+print(year_list)
+nYears = len(year_list)
+fname_list = []
+
+
+nprofile_hdf = h5py.File(fname_nprofile, 'r')
+nprof_mat = np.array(nprofile_hdf.get('n_profile_matrix'))
+zprof_mat = np.array(nprofile_hdf.get('z_profile'))
+
+Depths0 = len(zprof_mat)
+nProfiles = len(nprof_mat)
+sim0 = create_sim(fname_config)
+dz = sim0.dz
+z_prof_in = np.arange(min(zprof_mat), max(zprof_mat), dz)
+tx_signal0 = create_tx_signal(fname_config)
+freq_space = tx_signal0.freq_space
+
+nDepths = len(z_prof_in)
+
+n_matrix_yr = np.ones((nYears, nDepths))
+for i in range(nYears):
+    jj = util.findNearest(date_arr, year_list[i])
+    year_id_list.append(jj)
+nProfiles_sim = nYears
+
+z_depths = create_transmitter_array(fname_config)
+
+
+
+for i in year_id_list:
+    fname_output_npy = 'sim_ascan_CFM_' + str(i).zfill(3) + '.npy'
+    fname_output_h5 = 'sim_ascan_CFM_' + str(i).zfill(3) + '.h5'
+
+    create_spectrum(fname_config=fname_config,
+                    nprof_data=nprof_mat[i], zprof_data=zprof_mat,
+                    fname_output_npy=fname_output_npy,
+                    fname_output_h5=fname_output_h5)
+    for j in range(len(freq_space)):
+        for k in range(len(z_depths)):
+            suffix =  str(i).zfill(3)+ 'z_' + str(round(z_depths[k],2)).zfill(3) + 'm_f_' + str(round(freq_space[j],2)).zfill(2)
+            jobname = dir_sim_path + suffix
+            fname_sh_in = 'ascan_2CFM_' + suffix + '.sh'
+            fname_sh_out = dir_sim_path + 'ascan_2CFM_' + suffix + '.out'
+            cmd = 'python runSim_ascan.py ' + fname_config + ' ' + fname_output_npy + ' '
+            cmd += fname_nprofile + ' ' + str(i) + ' ' + str(j) + ' ' + str(k)
+            make_job(fname_shell=fname_sh_in, fname_outfile=fname_sh_out, jobname=jobname, command=cmd)
+            submit_job(fname_sh_in)
