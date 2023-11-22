9f02f375130b19a06b2ca7416cc29eddd8613363
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2023-11-22 16:20:38 +0100

    remove stupid error

--- firn_variance_study/add_spectrum_to_hdf.py
+++ firn_variance_study/add_spectrum_to_hdf.py
@@ -14,11 +14,11 @@ fin_list = open(fname_list, 'r')
 cols0 = fin_list.readline().split()
 dir_sim_path = cols0[0]
 fname_hdf0 = cols0[1]
-fname_npy0 = cols0[1]
+fname_npy0 = cols0[2]
 
 fname_hdf = dir_sim_path + fname_hdf0
 fname_npy = dir_sim_path + fname_npy0
-
+print(fname_npy)
 cols1 = fin_list.readline().split()
 nTx = int(cols1[0])
 nRx = int(cols1[1])
@@ -32,18 +32,43 @@ for line in fin_list:
     cols_l = line.split()
     ii_freq = int(cols_l[0])
     freq_ii = float(cols_l[1])
-    fname_npy_ii = dir_sim_path + cols_l[2]
+    fname_spectrum_ii = dir_sim_path + cols_l[2]
 
-    spectrum_i = np.load(fname_npy_ii, 'r')
-    spectrum[:,:,ii_freq] = spectrum_i
+    if fname_spectrum_ii[-3:] == 'npy':
+        spectrum_i = np.load(fname_spectrum_ii, 'r')
+        spectrum[:, :, ii_freq] = spectrum_i
+    elif fname_spectrum_ii[-3:] == 'txt':
+        spectrum_i = np.zeros((nTx, nRx),dtype='complex')
 
+        with open(fname_spectrum_ii, 'r') as fin:
+            for _ in range(2):
+                next(fin)
+            jj_rx = 0
+            for line in fin:
+                cols = line.split()
+                rx_x = float(cols[0])
+                rx_z = float(cols[1])
+                amp_rx_re = float(cols[2])
+                amp_rx_im = float(cols[3])
+                amp_rx = amp_rx_re + 1j*amp_rx_im
+
+                spectrum_i[0,jj_rx] = amp_rx
+                jj_rx += 1
+            spectrum[:, :, ii_freq] = spectrum_i
+
+'''
 print(fname_hdf)
-ascan_in = ascan()
-print('check check')
-ascan_in.load_from_hdf(fname_hdf)
-ascan_in.save_spectrum(fname_npy)
-fin_list.close()
-
+with h5py.File(fname_hdf, 'a') as hdf_in:
+    hdf_in.create_dataset('rxSpectrum', data=spectrum)
+    pulse_arr = np.zeros((nTx, nRx, nSamples),dtype='complex')
+    for i in range(nTx):
+        for j in range(nRx):
+            spectrum_ij = spectrum[i,j]
+            spectrum_ij_flip = np.flip(spectrum_ij)
+            pulse_rx = np.fft.ifft(spectrum_ij_flip)
+            pulse_rx[i,j] = pulse_rx
+    hdf_in.create_dataset('rxSignal', data=pulse_arr)
+'''
 #TODO: DELETE INDIVIDUAL NPY FILES
 #TODO: DELETE OUT FILES
 '''
