9abf4d4bd640e4490f41fd4963af9d2531a85d40
AlexKyriacou92 <kyriacou@uni-wuppertal.de>
2025-04-15 10:46:55 -0500

    genSim config2

--- firn_variance_study3/genSim_askaryan_config2.py
+++ firn_variance_study3/genSim_askaryan_config2.py
@@ -6,17 +6,18 @@ import time
 import datetime
 import h5py
 import configparser
+from os import system, popen
 
 from os import system
 from ku_scripting import *
 
 sys.path.append('../')
 
-from data import create_sim, create_rxList_from_file, create_tx_signal, create_transmitter_array
 from data import create_ascan_hdf, ascan
 import util
-from data import create_transmitter_array_from_file
+from data import create_sim, create_rxList_from_file, create_tx_signal, create_transmitter_array
 from Askaryan_Signal import create_pulse as create_pulse_askaryan, TeV
+from transmitter import tx_signal
 
 '''
 This Script is utilized for a study on how changes in firn density affects 
@@ -45,34 +46,135 @@ I haven't yet worked out the balance between elegant/simple/readable code
 and code that gets what I need done in the here and now.
 '''
 
-def create_pulse_askaryan_config(fname_config):
+
+def countuserjobs():
+    # Counts the number of jobs that is running under your username
+    username = popen('whoami').read()[:-1]
+    # print(username)
+    cmd = r'squeue | grep "' + username + r'" | wc -l'
+    # print(cmd)
+    try:
+        output = int(subprocess.check_output(cmd, shell=True))
+    except:
+        output = 0
+    return output
+
+
+def create_ascan_hdf2(fname_config, tx_signal, z_tx, nprof_data, zprof_data, fname_output):
+    sim = create_sim(fname_config)
+    rxList = create_rxList_from_file(fname_config)
+    # tx_depths = create_transmitter_array(fname_config)
+    tx_depths = [z_tx]
+
+    output_hdf = h5py.File(fname_output, 'w')
+    output_hdf.attrs["iceDepth"] = sim.iceDepth
+    output_hdf.attrs["iceLength"] = sim.iceLength
+    output_hdf.attrs["airHeight"] = sim.airHeight
+    output_hdf.attrs["dx"] = sim.dx
+    output_hdf.attrs["dz"] = sim.dz
+    # output_hdf.attrs['n0'] = sim.n0
+
+    output_hdf.attrs["Amplitude"] = tx_signal.amplitude
+    output_hdf.attrs["freqCentral"] = tx_signal.frequency
+    output_hdf.attrs["Bandwidth"] = tx_signal.bandwidth
+    output_hdf.attrs["freqMax"] = tx_signal.freqMax
+    output_hdf.attrs["freqMin"] = tx_signal.freqMin
+    output_hdf.attrs["freqSample"] = tx_signal.fsample
+    output_hdf.attrs["freqNyquist"] = tx_signal.freq_nyq
+    output_hdf.attrs["tCentral"] = tx_signal.t_centre
+    output_hdf.attrs["tSample"] = tx_signal.tmax
+    output_hdf.attrs["dt"] = tx_signal.dt
+    output_hdf.attrs["nSamples"] = tx_signal.nSamples
+
+    output_hdf.create_dataset('n_profile', data=nprof_data)
+    output_hdf.create_dataset('z_profile', data=zprof_data)
+    output_hdf.create_dataset("source_depths", data=tx_depths)
+    output_hdf.create_dataset('tspace', data=tx_signal.tspace)
+    output_hdf.create_dataset('signalPulse', data=tx_signal.pulse)
+    output_hdf.create_dataset('signalSpectrum', data=tx_signal.spectrum)
+
+    rxList_positions = np.ones((len(rxList), 2))
+    for i in range(len(rxList)):
+        rx_i = rxList[i]
+        rxList_positions[i, 0] = rx_i.x
+        rxList_positions[i, 1] = rx_i.z
+
+    output_hdf.create_dataset("rxList", data=rxList_positions)
+    return output_hdf
+
+
+def create_pulse(fname_config):
     config = configparser.ConfigParser()
     config.read(fname_config)
-    ask_config = config['ASKARYAN']
+    config_tx_signal = config['TX_SIGNAL']
+    tx_mode = config_tx_signal['mode']
+    if tx_mode == 'gaussian':
+        tx_signal_out = create_tx_signal(fname_config)
+        tx_signal_out.get_gausspulse()
+        tx_signal_out.add_gaussian_noise()
+    elif tx_mode == 'impulse':
+        # tx_signal_out = create_tx_signal(fname_config)
+        fmin_BP = float(config_tx_signal['freqMin_BP'])
+        fmax_BP = float(config_tx_signal['freqMax_BP'])
+        t_centre = float(config_tx_signal['t_centre'])
+        dt = float(config_tx_signal['dt'])
+        band = float(fmax_BP - fmin_BP)
+        t_max = float(config_tx_signal['t_max'])
+        freq_centre = float(config_tx_signal['freq_centre'])
 
-    dtheta_v = float(ask_config['dtheta_v'])
-    E_nu_eV = float(ask_config['showerEnergy'])
-    E_nu_TeV = E_nu_eV/TeV
-    R_alpha = float(ask_config['attenuationEquivalent'])
-    tx_signal_out = create_tx_signal(fname_config)
-    tspace = tx_signal_out.tspace
-    tmax = tx_signal_out.tmax
-    t_center = tx_signal_out.t_centre
+        tx_signal_out = tx_signal(frequency=freq_centre, bandwidth=band, t_centre=t_centre, dt=dt, tmax=t_max,
+                                  amplitude=1)
+        tx_signal_out.set_impulse()
+        tx_signal_out.apply_bandpass(fmin=fmin_BP, fmax=fmax_BP)
+        # tx_signal_out.add_gaussian_noise()
+    elif tx_mode == 'askaryan':
+        ask_config = config['ASKARYAN']
 
-    pulse_ask_out, tspace_ask = create_pulse_askaryan(Esh=E_nu_TeV, dtheta_v=dtheta_v, R_alpha=R_alpha,
-                                                      t_min=-1*t_center/1e3, t_max=(tmax-t_center)/1e3,
-                                                      fs=tx_signal_out.fsample*1e3)
-    pulse_ask_interp = np.interp(tspace, tspace_ask, pulse_ask_out)
-    tx_signal_out.set_pulse(pulse_ask_interp, tspace)
+        dtheta_v = float(ask_config['dtheta_v'])
+        E_nu_eV = float(ask_config['showerEnergy'])
+        E_nu_TeV = E_nu_eV / TeV
+        R_alpha = float(ask_config['attenuationEquivalent'])
+        tx_signal_out = create_tx_signal(fname_config)
+        tspace = tx_signal_out.tspace
+        tmax = tx_signal_out.tmax
+        t_center = tx_signal_out.t_centre
+
+        pulse_ask_out, tspace_ask = create_pulse_askaryan(Esh=E_nu_TeV, dtheta_v=dtheta_v, R_alpha=R_alpha,
+                                                          t_min=-1 * t_center / 1e3, t_max=(tmax - t_center) / 1e3,
+                                                          fs=tx_signal_out.fsample * 1e3)
+        pulse_ask_interp = np.interp(tspace, tspace_ask, pulse_ask_out)
+        tx_signal_out.set_pulse(pulse_ask_interp, tspace)
     return tx_signal_out
 
-def create_spectrum(fname_config, nprof_data, zprof_data, fname_output_h5):
-    tx_signal_out = create_pulse_askaryan_config(fname_config)
-    hdf_ascan = create_ascan_hdf(fname_config=fname_config,
-                                 tx_signal=tx_signal_out,
-                                 nprof_data=nprof_data,
-                                 zprof_data=zprof_data,
-                                 fname_output=fname_output_h5)
+
+def create_spectrum(fname_config, nprof_data, zprof_data, fname_output_h5, z_tx=None):
+    '''
+    TO DO: change this to only have 1 tx
+    Parameters
+    ----------
+    fname_config
+    nprof_data
+    zprof_data
+    fname_output_h5
+
+    Returns
+    -------
+
+    '''
+
+    tx_signal_out = create_pulse(fname_config)
+    if z_tx == None:
+        hdf_ascan = create_ascan_hdf(fname_config=fname_config,
+                                     tx_signal=tx_signal_out,
+                                     nprof_data=nprof_data,
+                                     zprof_data=zprof_data,
+                                     fname_output=fname_output_h5)
+    else:
+        hdf_ascan = create_ascan_hdf2(fname_config=fname_config,
+                                      z_tx=z_tx, tx_signal=tx_signal_out,
+                                      nprof_data=nprof_data,
+                                      zprof_data=zprof_data,
+                                      fname_output=fname_output_h5)
     hdf_ascan.close()
     return tx_signal_out
 
@@ -104,7 +206,6 @@ with open(fname_nprofile_all) as all_profiles:
 
 nProfiles_all = len(nprofile_list)
 
-t_wait = 10 # 10 Minutes
 
 now = datetime.datetime.now()
 datetime_str = now.strftime('%y%m%d_%H%M%S')
@@ -115,10 +216,16 @@ f_log = open('log_file_' + datetime_str + '.txt', 'w')
 f_log.write('sim_num\tyear\tmonth\tpath2nprofiles\tfname_profile\tpath2hdf\tfname_hdf\tfname_config')
 f_log.close()
 
-#ii_nprof_start = 35*12 + 1
+#Waiting Time
+nMinutes = 120
+t_60 = 60.
+max_wait_time = nMinutes * t_60
+t_wait_it = 10.
+
 ii_nprof_start = 30*12 + 1
-ii_nprof_end = ii_nprof_start + 12*10
-#ii_nprof_end = nProfiles_all
+ii_nprof_end = 30*12 + 2
+#ii_nprof_end = ii_nprof_start + 12*10
+
 for ii_nprof in range(ii_nprof_start, ii_nprof_end):
     print('run simulation for', year_l[ii_nprof], ' ', month_l[ii_nprof])
     fname_nprof = nprofile_list[ii_nprof]
@@ -132,7 +239,19 @@ for ii_nprof in range(ii_nprof_start, ii_nprof_end):
     fname_npy0 = fname_body + '.npy'
     fname_hdf = dir_sim_path + fname_hdf0
 
-    txList = create_transmitter_array_from_file(fname_config)
+    tx_config = config['TRANSMITTER']
+    for key in tx_config.keys():
+        print(key)
+    txList = []
+    if 'source_depth' in tx_config.keys():
+        txList.append(float(tx_config['source_depth']))
+    elif 'fname_transmitters' in tx_config.keys():
+        txList = create_transmitter_array(fname_config)
+    elif 'source_depth' in tx_config.keys() and 'fname_transmitters' in tx_config.keys():
+        txList = create_transmitter_array(fname_config)
+    else:
+        print('error, config[TRANSMITTER] in config file must have fname_transmitters or source_depth')
+        exit()
     nTx = len(txList)
     tx_signal_in = create_spectrum(fname_config=fname_config,
                                    nprof_data=nprof_data, zprof_data=zprof_data,
@@ -162,7 +281,7 @@ for ii_nprof in range(ii_nprof_start, ii_nprof_end):
     fout_list.write(dir_sim_path+ '\t' + fname_hdf0 + '\t' + fname_npy0 +'\n')
     fout_list.write(str(nTx) + '\t' + str(nRx) + '\t' + str(nSamples) + '\n')
     fout_list.write('ID_TX\tID_Freq\tFreq_GHz\tfname_npy\n')
-
+    #TODO: Make this more multi-TX compatible, as in
     for ii_tx in range(nTx):
         z_tx = txList[ii_tx]
         for ii_freq in range(ii_min, ii_max):
@@ -211,26 +330,24 @@ for ii_nprof in range(ii_nprof_start, ii_nprof_end):
     f_log.write(line_out)
     f_log.close()
     
-    t_waiting = t_wait
+    t_waiting = t_wait_it
     proceed_bool = False
     while proceed_bool == False:
         nJobs = countjobs()
         print('nJobs = ', nJobs)
-        if t_waiting < 15*60:
+        if t_waiting < max_wait_time:
             if nJobs > 0:
-                print('Waiting for', t_wait, 's')
-                time.sleep(t_wait)
-                t_waiting += t_wait
+                print('Waiting for', t_wait_it, 's', ', total wait = ', t_waiting, 's')
+                time.sleep(t_wait_it)
+                t_waiting += t_wait_it
             else:
                 print('Jobs complete, proceed')
                 proceed_bool = True
         else:
-            print('Time out! Not all jobs terminatied after', t_wait*nLimit, 's')
+            print('Time out! Not all jobs terminatied after', max_wait_time, 's')
             print('Abort, shut down all remaining jobs')
             system('./kill_jobs.sh')
-            print('yiasou f**ken')
             exit()
     system('python add_spectrum_to_hdf.py ' + dir_sim_path + fname_list)
     system('python add_npy_to_hdf.py ' + dir_sim_path)
     print('Sim complete:', year_l[ii_nprof], ' ', month_l[ii_nprof], '\n')
-    #TODO: Add a way to check that the simulation worked and produced valid results
